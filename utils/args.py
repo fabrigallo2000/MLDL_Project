37bfa06916b50fbb0502e2c80af4dc3611f5f45e
Federico <federico.dagostino@hotmail.it>
2023-07-02 14:23:29 +0200

    aggiunta aval in args

--- utils/args.py
+++ utils/args.py
@@ -27,6 +27,8 @@ def get_parser():
     parser.add_argument('--POC', action='store_true', default=False)
     parser.add_argument('--SPEC',action='store_true',default=False)
     parser.add_argument('--noise',action='store_true',default=False)
+    parser.add_argument('--eval',action='store_false',default=True)
+
 
     
     return parser
